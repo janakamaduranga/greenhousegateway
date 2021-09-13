362f26b06679faa798af01ad2879334863dec22e
Janaka <janaka.maduranga@gmai.com>
2021-09-13 11:21:10 +0530

    First commit

---
+++ src/main/java/com/green/house/greenhousegateway/GreenhousegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.green.house.greenhousegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhousegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhousegatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/house/greenhousegateway/GreenhousegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.house.greenhousegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhousegatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
